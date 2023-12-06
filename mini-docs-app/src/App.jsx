89d4ece0af0c8f9b9a5d4875c43371ba67f6d84d
Ankush2107 <ankush.kumar210701@gmail.com>
2023-12-06 23:39:27 +0530

    Creating a mini docs app using vite

---
+++ mini-docs-app/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+function App() {
+
+  return (
+    <div className='relative w-full h-screen bg-zinc-800'>
+      <h1 className='absolute left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter'>Docs</h1>
+    </div>
+  )
+}
+
+export default App
